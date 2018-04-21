6ccf75241215a328d98eb6f638afd6645f5262b4
mad7m_000 <mad7mile@gmail.com>
2018-04-21 22:52:00 +0300

    Initial commit

---
+++ app/src/main/java/com/mad7m_000/crime/criminalintent/CrimeActivity.java
@@ -0,0 +1,13 @@
+package com.mad7m_000.crime.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
